12ca99a213f497efa324696a8a93e5eb7249c1b9
Miguel Salazar <yobebe@hotmail.com.ar>
2023-06-26 16:59:52 -0300

    haciendo algunos cambios en el codigo

--- clima.js
+++ clima.js
@@ -3,9 +3,8 @@ const api = {
   key: '9e122cd782b2d0333f5fe4e7fa192062',
   url: 'https://api.openweathermap.org/data/2.5/weather'
 };
-// Voy a conectar los "ID".
-const card = document.getElementById('card');
 
+const card = document.getElementById('card');
 const city = document.getElementById('city');
 const date = document.getElementById('date');
 const tempImg = document.getElementById('temp-img');
@@ -13,28 +12,28 @@ const temp = document.getElementById('temp');
 const weather = document.getElementById('weather');
 const range = document.getElementById('range');
 
-// Voy a crear los condicionales que actualizan y muestran datos e imágenes del clima.
 function actualizarClima(data) {
-  const clima = toCelsius(data.main.clima);
+  const clima = toCelsius(data.main.temp);
   let imagenesDelClima = [];
-}
 
   if (clima >= 30) {
     imagenesDelClima.push('img/calor.png');
     imagenesDelClima.push('img/verano.png');
-  } else if (clima === 'nublado' || clima === 'lluvia') {
-    imagenesDelClima.push('img/lluvia.png');
-    imagenesDelClima.push('img/nubes.png');
-  } else if (clima === 'tormenta' || clima === 'lluvia con sol') {
-    imagenesDelClima.push('img/sol-nubes-y-lluvia.jpg');
-    imagenesDelClima.push('img/tormenta.png');
   } else if (clima <= 10 && clima > 0) {
     imagenesDelClima.push('img/frio.png');
     imagenesDelClima.push('img/invierno.png');
-  } else if (clima >= 0) {
-    imagenesDelClima.push('img/muñeco-de-nieve.png');
-    imagenesDelClima.push('img/frio.png');
-  } else if (clima <= 20) {
+  } else if (clima >= 0 && clima < 30) {
     imagenesDelClima.push('img/sol.png');
-    imagenesDelClima.push
+  } else {
+    imagenesDelClima.push('img/default.png'); // Imagen por defecto si no se cumple ninguna condición.
   }
+
+  // Actualizar elementos del DOM con los datos y las imágenes del clima.
+  temp.innerHTML = clima + '°C';
+  tempImg.src = imagenesDelClima[0]; // Mostrar la primera imagen del clima.
+}
+
+// Función de conversión de Kelvin a Celsius.
+function toCelsius(kelvin) {
+  return Math.round(kelvin - 273.15);
+}
